948166882ad43cc024f399e8b0472e6abc65b6ca
Stanislav Chlebec <stanislav.chlebec@pantheon.tech>
2018-10-03 08:45:16 +0200

    Remove false positive tests
    Originally abandoned by author Matej Klotton - original Change-Id: I7030428ce113baeafbde205bcb90c85c9d56daf9.
    
    Change-Id: I6f6eeaa49965b96c58d5012b8a5625bc42573693
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>
    Issue-ID: APPC-1204
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

--- plans/appc/healthcheck/health_check.sh
+++ plans/appc/healthcheck/health_check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ###############################################################################
-# Copyright 2018 AT&T
+# Copyright 2017 Huawei Technologies Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
