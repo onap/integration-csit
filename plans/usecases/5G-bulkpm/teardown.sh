40d938ccb2580c66191eada3517c0c5edfae1695
LiamBurke <Liam.Burke@ericsson.com>
2018-10-03 15:02:12 +0000

    Updates to the Data File Collector config json
    Change-Id: I975c8c83c6c1e539e739287c3e9dd91f812b4a6f
    Issue-ID: INT-649
    Signed-off-by: LiamBurke <liam.burke@ericsson.com>

--- plans/usecases/5G-bulkpm/teardown.sh
+++ plans/usecases/5G-bulkpm/teardown.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Starting teardown script"
 docker exec dfc /bin/sh -c "cat /opt/log/application.log"
-sleep 3
 kill-instance.sh $DMAAP
 kill-instance.sh $KAFKA
 kill-instance.sh $ZOOKEEPER
