e2fa9d46aa2242c280be9de6d3a1d60615e033f5
Dileep Ranganathan <dileep.ranganathan@intel.com>
2018-10-04 15:34:34 -0700

    OOF SMS CSIT
    Added CSIT support for HAS and OSDF with SMS
    Bring up dependencies like sms service and related containers
    Preload secrets
    Removed clear text secrets from config files
    Deleted unused osdf folders inside has folders.
    osdf uses separate folders now.
    
    Change-Id: Ie534e13b87a98de9f3ab1ed0be66f5f699353d93
    Issue-ID: OPTFRA-343
    Signed-off-by: Dileep Ranganathan <dileep.ranganathan@intel.com>

--- plans/optf-has/has/setup.sh
+++ plans/optf-has/has/setup.sh
@@ -14,7 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+echo "# aaf-sms setup.sh script";
+source ${WORKSPACE}/plans/aaf/sms-test-plan/setup.sh
 
 #
 # add here eventual scripts needed for music
