be3ecea9c66d52f6ff23bbee0320fa73630cbd75
sv764t <sv764t@att.com>
2018-10-25 01:42:52 -0400

    Change Management Schedule Optimization
    Adding scripts and robot test suites for cmso as per CSIT procedure
    Fixing the start up scripts to use docker
    Removing .pyc files
    
    Change-Id: I61492abd5d5060b5a773dec74320f2ecfc0fcbf5
    Issue-ID: OPTFRA-382
    Signed-off-by: sv764t <sv764t@att.com>
    Signed-off-by: vrvarma <vv8305@att.com>

---
+++ scripts/optf-cmso/cmso/clone_cmso_and_change_dockercompose.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# -------------------------------------------------------------------------
+#   Copyright (c) 2018 AT&T Intellectual Property
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# -------------------------------------------------------------------------
+#
+
+
+echo "This is ${WORKSPACE}/scripts/cmso/clone_cmso_and_change_dockercompose.sh"
+
+
+
+
+# Clone cmso repo to get extra folder that has all needed to run docker with docker-compose to start DB and cmso-service and cmso-dbinit
+mkdir -p $WORKSPACE/archives/cmso-clone
+cd $WORKSPACE/archives/cmso-clone
+git clone --depth 1 https://gerrit.onap.org/r/optf/cmso -b master
+cd cmso/cmso-service/extra/docker
+
+# Pull the cmso docker image from nexus instead of local image by default in the docker-compose.yml
+sed -i '/image: onap\/optf-cmso-service/c\    image: nexus3.onap.org:10001\/onap\/optf-cmso-service' docker-compose.yml
+
+sed -i '/image: onap\/optf-cmso-dbinit/c\    image: nexus3.onap.org:10001\/onap\/optf-cmso-dbinit' docker-compose.yml
+
+
