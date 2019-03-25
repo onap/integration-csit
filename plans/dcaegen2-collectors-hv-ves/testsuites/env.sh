f344afa4b2b3c009823fb4895f22c9f839f1dbb0
Jakub Dudycz <jakub.dudycz@nokia.com>
2019-03-25 16:04:58 +0100

    Allow HV-VES tests to be run locally
    Change-Id: Id81e209c03486354a60b26c4fe426071080198f0
    Signed-off-by: Jakub Dudycz <jakub.dudycz@nokia.com>
    Issue-ID: DCAEGEN2-1369

---
+++ plans/dcaegen2-collectors-hv-ves/testsuites/env.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# ============LICENSE_START=======================================================
+# csit-dcaegen2-collectors-hv-ves
+# ================================================================================
+# Copyright (C) 2019 NOKIA
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+
+# using WORKSPACE variable defined in run-csit.sh
+export ROBOT_VARIABLES="--pythonpath ${WORKSPACE}/tests/dcaegen2-collectors-hv-ves/testcases/libraries"
+
+export JAVA_OPTS="-Dio.netty.leakDetection.level=paranoid"
+export CONSUL_HOST="consul-server"
+export CONFIG_BINDING_SERVICE="config-binding-service"
+
+export DOCKER_REGISTRY="nexus3.onap.org:10001"
+export DOCKER_REGISTRY_PREFIX="${DOCKER_REGISTRY}/"
+export CONTAINERS_NETWORK="hv-ves-default"
+export HV_VES_SERVICE_NAME="hv-ves-collector"
+export UNENCRYPTED_HV_VES_SERVICE_NAME="unencrypted-hv-ves-collector"
+
+export HV_VES_GROUP_ID="org.onap.dcaegen2.collectors.hv-ves"
+export HV_VES_HOSTNAME="dcae-hv-ves-collector"
+export HV_VES_COLLECTOR_NAMESPACE="onap"
+export HV_VES_HEALTHCHECK_CMD=$(curl --request GET --fail --silent --show-error localhost:6060/health/ready && nc -vz localhost 6061)
+export HV_VES_VERSION="1.1-SNAPSHOT"
+export HV_VES_IMAGE="hv-collector-main"
+export DCAE_APP_SIMULATOR_IMAGE="hv-collector-dcae-app-simulator"
+export XNF_SIMULATOR_IMAGE="hv-ves-collector-xnf-simulator"
+
+
--- plans/dcaegen2-collectors-hv-ves/testsuites/setup.sh
+++ plans/dcaegen2-collectors-hv-ves/testsuites/setup.sh
@@ -19,14 +19,13 @@
 
 set -euo pipefail
 
-if [[ $# -eq 1 ]] && [[ $1 == "local-test-run" ]]; then
+RUN_CSIT_LOCAL=${RUN_CSIT_LOCAL:-false}
+
+if ${RUN_CSIT_LOCAL} ; then
   echo "Building locally - assuming all dependencies are installed"
-  export DOCKER_REGISTRY=""
-  export DOCKER_REGISTRY_PREFIX=""
-  export WORKSPACE=$(git rev-parse --show-toplevel)
+  source env_local.sh
 else
   echo "Default run - install all dependencies"
-
   pip uninstall -y docker-py
   pip install docker
 
@@ -34,15 +33,12 @@ else
   COMPOSE_LOCATION='/usr/local/bin/docker-compose'
   sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) -o ${COMPOSE_LOCATION}
   sudo chmod +x ${COMPOSE_LOCATION}
-
-  export DOCKER_REGISTRY="nexus3.onap.org:10001"
-  export DOCKER_REGISTRY_PREFIX="${DOCKER_REGISTRY}/"
+  source env.sh
 fi
 
 echo "Removing not used docker networks"
 docker network prune -f
 
-export CONTAINERS_NETWORK=ves-hv-default
 echo "Creating network for containers: ${CONTAINERS_NETWORK}"
 docker network create ${CONTAINERS_NETWORK}
 
@@ -53,5 +49,3 @@ cd ../..
 docker-compose up -d
 
 mkdir -p ${WORKSPACE}/archives/containers_logs
-
-export ROBOT_VARIABLES="--pythonpath ${WORKSPACE}/tests/dcaegen2-collectors-hv-ves/testcases/libraries"
