5d1b00692e2acbcc17d6b1748d03686c587a81dc
Filip Krzywka <filip.krzywka@nokia.com>
2019-04-02 13:03:56 +0200

    Test configuration updates in CSITs
    Change-Id: Ia02938d078baaf60b430ec4ca45c6ad9f24663a1
    Issue-ID: DCAEGEN2-1381
    Signed-off-by: Filip Krzywka <filip.krzywka@nokia.com>

--- tests/dcaegen2-collectors-hv-ves/testcases/libraries/HttpRequests.py
+++ tests/dcaegen2-collectors-hv-ves/testcases/libraries/HttpRequests.py
@@ -33,4 +33,4 @@ def session_without_env():
 def checkStatusCode(status_code, server_name):
     if status_code not in valid_status_codes:
         logger.error("Response status code from " + server_name + ": " + str(status_code))
-        raise (Exception(server_name + " returned status code " + status_code))
+        raise (Exception(server_name + " returned status code " + str(status_code)))
