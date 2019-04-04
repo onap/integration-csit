ae2264c868b5dd308a3a2087aacc2be470001468
Filip Krzywka <filip.krzywka@nokia.com>
2019-04-04 14:22:30 +0200

    Split docker-compose file logs
    Change-Id: I5f9d91f871d898cb776e940e054c14f26872852d
    Issue-ID: DCAEGEN2-1359
    Signed-off-by: Filip Krzywka <filip.krzywka@nokia.com>

--- plans/dcaegen2-collectors-hv-ves/testsuites/teardown.sh
+++ plans/dcaegen2-collectors-hv-ves/testsuites/teardown.sh
@@ -36,7 +36,12 @@ set +e
 METRICS_FILE=${WORKSPACE}/archives/containers_logs/hv-ves-metrics.dump
 docker-compose exec hv-ves-collector curl -qs localhost:6060/monitoring/prometheus > ${METRICS_FILE}
 
-COMPOSE_LOGS_FILE=${WORKSPACE}/archives/containers_logs/docker-compose.log
+CONTAINER_LOGS=${WORKSPACE}/archives/containers_logs/
+COMPOSE_LOGS_FILE=${CONTAINER_LOGS}/docker-compose.log
+
+docker-compose logs hv-ves-collector > ${CONTAINER_LOGS}/hv-ves-collector.log
+docker-compose logs unencrypted-hv-ves-collector > ${CONTAINER_LOGS}/unencrypted-hv-ves-collector.log
+docker-compose logs dcae-app-simulator > ${CONTAINER_LOGS}/dcae-app-simulator.log
 docker-compose logs > ${COMPOSE_LOGS_FILE}
 docker-compose down
 docker-compose rm -f
