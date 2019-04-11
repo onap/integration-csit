f3894693fb1a19ca5333994e1fc32a179b01859f
kurczews <krzysztof.kurczewski@nokia.com>
2019-04-11 12:51:35 +0200

    PRH simulators refactor
    Issue-ID: INT-989
    Change-Id: I87b9a998291ea55f422bce8b315d4b5f03950e2c
    Signed-off-by: kurczews <krzysztof.kurczewski@nokia.com>

--- tests/dcaegen2/prh-testcases/resources/simulator/httpServerLib.py
+++ tests/dcaegen2/prh-testcases/resources/simulator/httpServerLib.py
@@ -2,27 +2,39 @@ import _thread
 import ssl
 from http.server import HTTPServer
 
-
-def header_200_and_json(self):
+def set_response_200_ok(self, payload = None):
     self.send_response(200)
     self.send_header('Content-Type', 'application/json')
     self.end_headers()
+    if payload != None:
+        self.wfile.write(payload)
 
+def set_response_404_not_found(self):
+    self.send_response(404)
+    self.end_headers()
+
+def set_response_500_server_error(self):
+    self.send_response(500)
+    self.end_headers()
+
+def get_payload(self):
+    if self.headers['Content-Length'] == None:
+        raise Exception('Invalid payload, Content-Length not defined')
+
+    content_length = int(self.headers['Content-Length'])
+    return self.rfile.read(content_length)
 
 def start_http_endpoint(port, handler_class):
     _thread.start_new_thread(init_http_endpoints, (port, handler_class))
 
-
 def start_https_endpoint(port, handler_class, keyfile, certfile, ca_certs):
     _thread.start_new_thread(init_https_endpoints, (port, handler_class, keyfile, certfile, ca_certs))
-
 
 def init_http_endpoints(port, handler_class, server_class=HTTPServer):
     server = server_class(('', port), handler_class)
     sa = server.socket.getsockname()
     print("Serving HTTP on", sa[0], "port", sa[1], "for", handler_class, "...")
     server.serve_forever()
-
 
 def init_https_endpoints(port, handler_class, keyfile, certfile, ca_certs, server_class=HTTPServer):
     server = server_class(('', port), handler_class)
