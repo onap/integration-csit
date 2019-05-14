0bafc7aa87475602eefae3cedc4589351ac7a2a1
grabinsk <maciej.grabinski@nokia.com>
2019-05-14 10:53:52 +0200

    [WIP] Use single PRH container with ssl configuration
    Change-Id: If68ceab0bea7dbc10db647af7530b39b44729c0c
    Issue-ID: INT-1076
    Signed-off-by: grabinsk <maciej.grabinski@nokia.com>

--- plans/dcaegen2/prh-testsuites/setup.sh
+++ plans/dcaegen2/prh-testsuites/setup.sh
@@ -3,7 +3,6 @@
 source ${SCRIPTS}/common_functions.sh
 
 export PRH_SERVICE="prh"
-export SSL_PRH_SERVICE="ssl_prh"
 export DMAAP_SIMULATOR="dmaap_simulator"
 export AAI_SIMULATOR="aai_simulator"
 export CONSUL="consul"
@@ -23,7 +22,6 @@ docker-compose up -d --build
 
 # Extract docker images IPs
 PRH_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${PRH_SERVICE})
-SSL_PRH_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${SSL_PRH_SERVICE})
 DMAAP_SIMULATOR_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${DMAAP_SIMULATOR})
 AAI_SIMULATOR_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${AAI_SIMULATOR})
 CONSUL_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONSUL})
@@ -31,7 +29,6 @@ CONSUL_CONFIG_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{
 CBS_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CBS})
 
 bypass_ip_adress ${PRH_IP}
-bypass_ip_adress ${SSL_PRH_IP}
 bypass_ip_adress ${DMAAP_SIMULATOR_IP}
 bypass_ip_adress ${AAI_SIMULATOR_IP}
 bypass_ip_adress ${CONSUL_IP}
@@ -39,7 +36,6 @@ bypass_ip_adress ${CONSUL_CONFIG_IP}
 bypass_ip_adress ${CBS_IP}
 
 echo PRH_IP=${PRH_IP}
-echo SSL_PRH_IP=${SSL_PRH_IP}
 echo DMAAP_SIMULATOR_IP=${DMAAP_SIMULATOR_IP}
 echo AAI_SIMULATOR_IP=${AAI_SIMULATOR_IP}
 echo CONSUL_IP=${CONSUL_IP}
@@ -48,7 +44,6 @@ echo CBS_IP=${CBS_IP}
 
 # Wait for initialization of PRH services
 wait_for_service_init localhost:8100/heartbeat
-wait_for_service_init localhost:8200/heartbeat
 
 # #Pass any variables required by Robot test suites in ROBOT_VARIABLES
 ROBOT_VARIABLES="-v DMAAP_SIMULATOR_SETUP:${DMAAP_SIMULATOR_IP}:2224 -v AAI_SIMULATOR_SETUP:${AAI_SIMULATOR_IP}:3335 -v CONSUL_SETUP:${CONSUL_IP}:8500"
