b52c1a9b7f060b87429a38f41f7e0e41f2bdbe61
kaihlavi <l.kaihlavirt@partner.samsung.com>
2019-05-27 16:51:09 +0300

    Correct minor typos in script
    Issue-ID: SDC-2333
    Signed-off-by: kaihlavi <l.kaihlavirt@partner.samsung.com>
    Change-Id: I5d5d44cd8dfcb8d81250af182915c163efb9a576

--- scripts/sdc/docker_health.sh
+++ scripts/sdc/docker_health.sh
@@ -2,22 +2,21 @@
 
 curl localhost:9200/_cluster/health?pretty=true
 
-echo "BE health-Check:"
+echo "BE Health Check:"
 curl http://localhost:8080/sdc2/rest/healthCheck
 
 echo ""
 echo ""
-echo "FE health-Check:"
+echo "FE Health Check:"
 curl http://localhost:8181/sdc1/rest/healthCheck
-
 
 echo ""
 echo ""
 http_code=$(curl -o out.html -w '%{http_code}' -H "Accept: application/json" -H "Content-Type: application/json" -H "USER_ID: jh0003" http://localhost:8080/sdc2/rest/v1/user/demo;)
 if [[ ${http_code} != 200 ]]
 then
-    echo "Error [${http_code}] while user existance check"
+    echo "Error [${http_code}] while checking existence of user"
     return ${http_code}
 fi
-echo "check user existance: OK"
+echo "Check user existence: OK"
 
