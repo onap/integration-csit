61faacf6335b019ababad8f5e3eeed523c917837
kaihlavi <l.kaihlavirt@partner.samsung.com>
2019-05-30 11:54:31 +0300

    Add temporary kill script for SDC DCAE containers
     Currently it is possible to start SDC's DCAE-DS docker containers
     with docker_run.sh (see https://gerrit.onap.org/r/#/c/sdc/+/88549/)
     but the containers are not killed by corresponding
     kill_containers_and_remove_dataFolders.sh - this script provides
     the ability to kill them until image naming and tagging has more
     complete solution that should render this one obsolete
    
    Change-Id: I623db3ace0d4e14b925254bd21528bfa8e00f458
    Issue-ID: SDC-2342
    Signed-off-by: kaihlavi <l.kaihlavirt@partner.samsung.com>

---
+++ scripts/sdc/kill_dcae_containers.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright 2019 © Samsung Electronics Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Note! This is only temporary solution for killing SDC DCAE plugin's
+# docker containers that must be currently used whenever docker_run.sh is used
+# with -dcae option - See SDC-2338 for related image naming issue 
+#
+# DCAE plugin-related parts will also have to be refactored under dedicated
+# directories in the future
+#
+
+echo "This is ${WORKSPACE}/scripts/sdc/kill_dcae_containers.sh"
+
+# kill and remove all sdc dcae dockers
+docker stop $(docker ps -a -q --filter="name=dcae")
+docker rm $(docker ps -a -q --filter="name=dcae")
