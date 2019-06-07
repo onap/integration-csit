b9c3e0b28b40e6fc7043ba210d11a273734ca6c2
Bilal A <bilal@research.att.com>
2019-06-07 21:25:59 +0000

    Pull correct docker images for master branch
    Enables Policy CSITs for API, PAP, Distribution, Drools-applications and XACML-PDP
    to pull correct docker images based on the code branch.
    
    Issue-ID: POLICY-1796
    Signed-off-by: Bilal A <bilal@research.att.com>
    Change-Id: I6b99c8ae9cb78dd8ebddc522bc2a7080ec6e5735

--- plans/policy/drools-applications/setup.sh
+++ plans/policy/drools-applications/setup.sh
@@ -16,11 +16,19 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # ============LICENSE_END=========================================================
+source ${SCRIPTS}/policy/config/policy-csit.conf
+export POLICY_MARIADB_VER
+echo ${GERRIT_BRANCH}
+echo ${POLICY_MARIADB_VER}
 
 echo "Uninstall docker-py and reinstall docker."
 pip uninstall -y docker-py
 pip uninstall -y docker
 pip install -U docker==2.7.0
+
+sudo apt-get -y install libxml2-utils
+export POLICY_DROOLS_APPS_VERSION="$(curl -q --silent https://git.onap.org/policy/drools-applications/plain/pom.xml?h=${GERRIT_BRANCH} | xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' -)"
+echo ${POLICY_DRROLS_APPS_VERSION}
 
 docker login -u docker -p docker nexus3.onap.org:10001
 
