2f39fae08c3c61939152aabe9b298834e7d2c4e9
Bilal A <bilal@research.att.com>
2019-06-17 15:51:08 +0000

    Fixing image download problem
    Adding fallback image name to download
    if image name present in pom.xml is not available
    in nexus repo.
    
    Issue-ID: POLICY-1864
    Signed-off-by: Bilal A <bilal@research.att.com>
    Change-Id: I0f00faef1e33a27edd195de3eba82276ca38d501

--- plans/policy/distribution/setup.sh
+++ plans/policy/distribution/setup.sh
@@ -22,8 +22,10 @@ echo $GERRIT_BRANCH
 
 sudo apt-get -y install libxml2-utils
 export POLICY_DISTRIBUTION_VERSION="$(curl --silent https://git.onap.org/policy/distribution/plain/pom.xml?h=${GERRIT_BRANCH} | xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' -)"
+export POLICY_DISTRIBUTION_VERSION=$POLICY_DISTRIBUTION_VERSION"-latest"
 echo $POLICY_DISTRIBUTION_VERSION
 docker run -d --name policy-distribution -p 6969:6969 -it nexus3.onap.org:10001/onap/policy-distribution:$POLICY_DISTRIBUTION_VERSION
+
 
 POLICY_DISTRIBUTION_IP=`get-instance-ip.sh policy-distribution`
 echo DISTRIBUTION IP IS ${POLICY_DISTRIBUTION_IP}
