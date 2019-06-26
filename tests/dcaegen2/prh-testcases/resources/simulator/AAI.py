f744fff9b482f528ba1525a99ce1180b5a131778
Marcin Migdal <marcin.migdal@nokia.com>
2019-06-26 11:38:24 +0200

    BBE re-registration enabling testcase Adding logger
    Change-Id: I7c01afc6bb443ad1cf57ebb99ff47b9a9e52280c
    Issue-ID: INT-1100
    Signed-off-by: Marcin Migdal <marcin.migdal@nokia.com>

--- tests/dcaegen2/prh-testcases/resources/simulator/AAI.py
+++ tests/dcaegen2/prh-testcases/resources/simulator/AAI.py
@@ -105,9 +105,9 @@ class AAIHandler(BaseHTTPRequestHandler):
                 else:
                     httpServerLib.set_response_404_not_found(self)
                     logger.info('AAIHandler GET /aai/v12/network/pnfs/pnf/' + pnf_name + ' -> 404 Not found, actual entries: ' + str(pnf_entries.keys()))
-            elif re.search('aai/v12/business/customers/customer/Demonstration/service-subscriptions/service-subscription/vFW/service-instances/service-instance/bbs_service', self.path):
+            elif re.search('aai/v12/network/pnfs/pnf/business/customers/customer/Demonstration/service-subscriptions/service-subscription/vFW/service-instances/service-instance/bbs_service', self.path):
                 httpServerLib.set_response_200_ok(self, payload = service_instance)
-                logger.debug('AAIHandler GET aai/v12/business/customers/customer/Demonstration/service-subscriptions/service-subscription/vFW/service-instances/service-instance/bbs_service -> 200 OK')
+                logger.debug('AAIHandler GET aai/v12/network/pnfs/pnf/business/customers/customer/Demonstration/service-subscriptions/service-subscription/vFW/service-instances/service-instance/bbs_service -> 200 OK')
             else:
                 httpServerLib.set_response_404_not_found(self)
                 logger.info('AAIHandler GET ' + self.path + ' -> 404 Not found')
