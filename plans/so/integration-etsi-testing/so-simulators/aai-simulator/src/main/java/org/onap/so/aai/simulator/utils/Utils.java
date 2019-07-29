334b52008bf68787ee14f2c0ff53a30cf73c418f
waqas.ikram <waqas.ikram@est.tech>
2019-07-29 17:36:08 +0000

    Adding project endpoint and https support
    Change-Id: Id5ee9f335ff8e5bb84b537bdc544b6608a35ecf0
    Issue-ID: SO-1953
    Signed-off-by: waqas.ikram <waqas.ikram@est.tech>

--- plans/so/integration-etsi-testing/so-simulators/aai-simulator/src/main/java/org/onap/so/aai/simulator/utils/Utils.java
+++ plans/so/integration-etsi-testing/so-simulators/aai-simulator/src/main/java/org/onap/so/aai/simulator/utils/Utils.java
@@ -5,47 +5,47 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
-package org.onap.so.aai.simulator.controller;
+package org.onap.so.aai.simulator.utils;
 
-import static org.onap.so.aai.simulator.utils.Constants.HEALTHY;
-import javax.ws.rs.core.MediaType;
-import org.onap.so.aai.simulator.utils.Constants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.onap.so.aai.simulator.utils.Constants.ERROR_MESSAGE;
+import static org.onap.so.aai.simulator.utils.Constants.ERROR_MESSAGE_ID;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.ResponseEntity;
 
 /**
  * @author waqas.ikram@ericsson.com
  *
  */
-@Controller
-@RequestMapping(path = Constants.BASE_URL)
-public class AaiSimulatorController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AaiSimulatorController.class);
+public class Utils {
 
-    @ResponseBody
-    @GetMapping(value = "/healthcheck", produces = MediaType.TEXT_PLAIN)
-    @ResponseStatus(code = HttpStatus.OK)
-    public String healthCheck() {
-        LOGGER.info("Running health check ...");
-        return HEALTHY;
+    private static final String EMPTY_STRING = "";
+
+    public static final String getResourceVersion() {
+        return System.currentTimeMillis() + EMPTY_STRING;
     }
+
+    public static ResponseEntity<?> getRequestErrorResponseEntity(final HttpServletRequest request) {
+        return new ResponseEntity<>(new RequestErrorBuilder().messageId(ERROR_MESSAGE_ID).text(ERROR_MESSAGE)
+                .variables(request.getMethod(), request.getRequestURI(),
+                        "Node Not Found:No Node of type service-instance found at: " + request.getRequestURI(),
+                        "ERR.5.4.6114")
+                .build(), HttpStatus.NOT_FOUND);
+    }
+
+
+    private Utils() {}
 
 }
