3af6eb2418305d00618d5b154ad4780dc58b1cf1
waqas.ikram <waqas.ikram@est.tech>
2019-08-02 17:36:09 +0000

    Adding operations endpoint for sdn-c simulator
    Change-Id: I02d6e988ff0f307bb8590eb3634505a01d656ea1
    Issue-ID: SO-1950
    Signed-off-by: waqas.ikram <waqas.ikram@est.tech>

--- plans/so/integration-etsi-testing/so-simulators/sdc-simulator/src/main/java/org/onap/so/sdcsimulator/controller/CatalogController.java
+++ plans/so/integration-etsi-testing/so-simulators/sdc-simulator/src/main/java/org/onap/so/sdcsimulator/controller/CatalogController.java
@@ -28,16 +28,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Waqas Ikram (waqas.ikram@est.tech)
  *
  */
-@RestController
+@Controller
 @RequestMapping(path = CATALOG_URL)
 public class CatalogController {
     private static final Logger LOGGER = LoggerFactory.getLogger(CatalogController.class);
