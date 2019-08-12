4a7516ba670c145b52cd0b2f3294aaf48473874d
kaihlavi <l.kaihlavirt@partner.samsung.com>
2019-08-12 15:44:26 +0300

    Remove redundant robot variable
     Remove the setting of unused BE_IP Robot variable and
     add copyright to script
    
    Change-Id: I6248ab469dfab5383f7ada852de3f39784dd3faa
    Issue-ID: SDC-2505
    Signed-off-by: kaihlavi <l.kaihlavirt@partner.samsung.com>

--- plans/sdc/uiSanity/setup.sh
+++ plans/sdc/uiSanity/setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+#
+# Copyright 2019 © Samsung Electronics Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 source ${WORKSPACE}/scripts/sdc/setup_sdc_for_sanity.sh tud
-
-BE_IP=`get-instance-ip.sh sdc-BE`
-echo BE_IP=${BE_IP}
-
-# Pass any variables required by Robot test suites in ROBOT_VARIABLES
-ROBOT_VARIABLES="-v BE_IP:${BE_IP}"
-
