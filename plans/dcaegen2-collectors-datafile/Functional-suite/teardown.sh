4d3685fe49398aa165660622f2730c48ce199f48
TamasBakai <tamas.bakai@est.tech>
2019-08-16 08:01:43 +0000

    CSIT uplift for CBS, generalized DFC, bugfixes, stability, traceability
    Issue-ID: DCAEGEN2-1719
    Change-Id: If5c96a7f205a34f78e50585a7187c5b9a3820023
    Signed-off-by: TamasBakai <tamas.bakai@est.tech>

--- plans/dcaegen2-collectors-datafile/Functional-suite/teardown.sh
+++ plans/dcaegen2-collectors-datafile/Functional-suite/teardown.sh
@@ -2,12 +2,28 @@
 
 #Stop dfc
 
-kill-instance.sh dfc_app
+docker exec -it dfc_app0 cat /var/log/ONAP/application.log > $WORKSPACE/archives/dfc_app0_application.log
+kill-instance.sh dfc_app0
 
 #Stop all simulators 
-
+kill-instance.sh dfc_cbs
+kill-instance.sh dfc_consul
 kill-instance.sh dfc_dr-sim
 kill-instance.sh dfc_dr-redir-sim
 kill-instance.sh dfc_mr-sim
-kill-instance.sh dfc_sftp-server
-kill-instance.sh dfc_ftpes-server-vsftpd
+kill-instance.sh dfc_sftp-server0
+kill-instance.sh dfc_sftp-server1
+kill-instance.sh dfc_sftp-server2
+kill-instance.sh dfc_sftp-server3
+kill-instance.sh dfc_sftp-server4
+kill-instance.sh dfc_sftp-server5
+kill-instance.sh dfc_ftpes-server-vsftpd0
+kill-instance.sh dfc_ftpes-server-vsftpd1
+kill-instance.sh dfc_ftpes-server-vsftpd2
+kill-instance.sh dfc_ftpes-server-vsftpd3
+kill-instance.sh dfc_ftpes-server-vsftpd4
+kill-instance.sh dfc_ftpes-server-vsftpd5
+
+
+
+
