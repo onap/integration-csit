7ed309ed2e61fb720e737ce2741e472bead33b43
TamasBakai <tamas.bakai@est.tech>
2019-08-21 07:20:26 +0000

    CSIT uplift for CBS, generalized DFC, bugfixes, stability, traceability
    Issue-ID: DCAEGEN2-1719
    Change-Id: If5c96a7f205a34f78e50585a7187c5b9a3820023
    Signed-off-by: TamasBakai <tamas.bakai@est.tech>

--- plans/dcaegen2-collectors-datafile/Functional-suite/teardown.sh
+++ plans/dcaegen2-collectors-datafile/Functional-suite/teardown.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
 
+set +e
 #Stop dfc
 
-kill-instance.sh dfc_app
+docker exec -t dfc_app0 cat /var/log/ONAP/application.log > $WORKSPACE/archives/dfc_app0_application.log
+kill-instance.sh dfc_app0
 
 #Stop all simulators 
-
+kill-instance.sh dfc_cbs
+kill-instance.sh dfc_consul
 kill-instance.sh dfc_dr-sim
 kill-instance.sh dfc_dr-redir-sim
 kill-instance.sh dfc_mr-sim
-kill-instance.sh dfc_sftp-server
-kill-instance.sh dfc_ftpes-server-vsftpd
+kill-instance.sh dfc_sftp-server0
+kill-instance.sh dfc_sftp-server1
+kill-instance.sh dfc_sftp-server2
+kill-instance.sh dfc_sftp-server3
+kill-instance.sh dfc_sftp-server4
+kill-instance.sh dfc_sftp-server5
+kill-instance.sh dfc_ftpes-server-vsftpd0
+kill-instance.sh dfc_ftpes-server-vsftpd1
+kill-instance.sh dfc_ftpes-server-vsftpd2
+kill-instance.sh dfc_ftpes-server-vsftpd3
+kill-instance.sh dfc_ftpes-server-vsftpd4
+kill-instance.sh dfc_ftpes-server-vsftpd5
+
+echo "List of Consul config before cleaning"
+ls -la $WORKSPACE/archives/dfc/integration/test/mocks/datafilecollector-testharness/simulator-group/consul/consul/*
+rm $WORKSPACE/archives/dfc/integration/test/mocks/datafilecollector-testharness/simulator-group/consul/consul/cbs_localhost_config.hcl
+
+
+set -e
+
+
