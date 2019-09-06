3ae5ef4c2f7a928bbd681eaa1183f0d746d48fd5
tragait <rahul.tyagi@est.tech>
2019-09-06 11:31:59 +0100

    Update config-over-netconf with el-alto images
    Change-Id: I06b7aaefe7dd40e20ac094074f5c0840867f1717
    Signed-off-by: tragait <rahul.tyagi@est.tech>
    Issue-ID: INT-1262

---
+++ plans/usecases/config-over-netconf/teardown.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SDNC_DOCKER_COMPOSE_PATH=$SDNC_DOCKER_PATH/docker-compose.yaml
+PNFSIM_DOCKER_COMPOSE_PATH=$INT_DOCKER_PATH/docker-compose.yml
+CDS_DOCKER_COMPOSE_PATH=$CDS_DOCKER_PATH/docker-compose.yaml
+
+docker-compose -f $SDNC_DOCKER_COMPOSE_PATH down
+docker-compose -f $PNFSIM_DOCKER_COMPOSE_PATH down
+docker-compose -f $CDS_DOCKER_COMPOSE_PATH down
+
+rm -rf $WORKSPACE/temp
