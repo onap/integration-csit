ed52fd562e4dce86b386e6803ee54e0acd26fefa
yanhuanwang <martin.c.yan@est.tech>
2019-09-13 14:19:05 +0200

    Fix bug of DFC daily jenkins job
    Change-Id: I59277bc4b259d1b91b3259e7985efca6b0639671
    Signed-off-by: yanhuanwang <martin.c.yan@est.tech>
    Issue-ID: DCAEGEN2-1702

--- plans/dcaegen2-collectors-datafile/Functional-suite/teardown.sh
+++ plans/dcaegen2-collectors-datafile/Functional-suite/teardown.sh
@@ -2,14 +2,15 @@
 
 running_containers=$(docker ps --filter name=dfc_ -qa)
 running_images=$(docker images -q)
+docker exec -it dfc_app0 cat /var/log/ONAP/application.log > $WORKSPACE/archives/dfc_app0_application.log
 
 if [ -z "$running_containers" ]
 then
     echo "No container requires termination"
 else
     echo "Stopping and removing containers"
-    docker stop $running_containers
-    docker rm $running_containers
-    docker rmi $running_images
+    #docker stop $running_containers
+    #docker rm $running_containers
+    #docker rmi $running_images
 fi
 
