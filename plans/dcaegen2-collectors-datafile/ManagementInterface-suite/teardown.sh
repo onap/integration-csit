f38c5aced00dc316aaba7893a1f7ab7c37ab23b9
yanhuanwang <martin.c.yan@est.tech>
2019-09-17 18:02:14 +0200

    Add application.log to console
    Change-Id: I460152ca18de3ec1ec7e7035a932aa0f1e1f6be0
    Signed-off-by: yanhuanwang <martin.c.yan@est.tech>
    Issue-ID: DCAEGEN2-1702

--- plans/dcaegen2-collectors-datafile/ManagementInterface-suite/teardown.sh
+++ plans/dcaegen2-collectors-datafile/ManagementInterface-suite/teardown.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set +e
-docker exec -it dfc_app0 cat /var/log/ONAP/application.log > $WORKSPACE/archives/dfc_app0_application.log
+docker exec dfc_app0 cat /var/log/ONAP/application.log > $WORKSPACE/archives/dfc_app0_application.log
 kill-instance.sh dfc_app0
 set -e
 exit 0
