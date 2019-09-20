2e2d97f371e5710db54ca5c9951bb0998af9bbf7
Petr Ospalý <p.ospaly@partner.samsung.com>
2019-09-20 11:41:23 +0200

    Update sdc nightly
    It is basically the same as sanity test. Updated with the sanity changes
    and removed unused variable for robot.
    
    Nightly tends to fail in jenkins but there is no reason for it if sanity
    succeeds. When run manually it works fine.
    
    Issue-ID: SDC-2558
    
    Signed-off-by: Petr Ospalý <p.ospaly@partner.samsung.com>
    Change-Id: I174867d85e5979b879f4b7de926e193d1453763c

--- plans/sdc/nightly/setup.sh
+++ plans/sdc/nightly/setup.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
+#
+# Copyright 2019 © Samsung Electronics Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# It can enable HTTPS for SDC sanity tests
+export SDC_TEST_HTTPS="${SDC_TEST_HTTPS:-false}"
+
+# By default all images are from remote upstream registry, this option
+# provides the chance to test locally built images
+export SDC_LOCAL_IMAGES="${SDC_LOCAL_IMAGES:-false}"
+
+# For this to take effect SDC_LOCAL_IMAGES must be enabled...
+#
+# The path to the local sdc git repo from which the local images have
+# been built - it also affects the tag used - if left empty *AND*
+# local images are used *AND* SDC_LOCAL_TAG is unset then the tag
+# will be set to: 'latest'
+#
+# BEWARE: Using local images with an incorrect git repo could lead to
+# problems...set SDC_LOCAL_GITREPO or GERRIT_BRANCH properly...
+export SDC_LOCAL_GITREPO="${SDC_LOCAL_GITREPO}"
+
+# For this to take effect SDC_LOCAL_IMAGES must be enabled...
+#
+# This will set the tag for local images - leaving this empty *AND*
+# with unset SDC_LOCAL_GITREPO the local images will fallback to the
+# tag: 'latest'
+export SDC_LOCAL_TAG="${SDC_LOCAL_TAG}"
 
 source ${WORKSPACE}/scripts/sdc/setup_sdc_for_sanity.sh tad
-
-BE_IP=`get-instance-ip.sh sdc-BE`
-echo BE_IP=${BE_IP}
-
-
-# Pass any variables required by Robot test suites in ROBOT_VARIABLES
-ROBOT_VARIABLES="-v BE_IP:${BE_IP}"
-
