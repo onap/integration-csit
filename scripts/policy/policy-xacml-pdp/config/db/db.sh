cb54dac0343ac722239eb2b477e3ead35e5c4c38
Bilal A <bilal@research.att.com>
2020-01-16 16:12:19 +0000

    Updating the jdk version from jdk8 to jdk11 for CSITs
    Removes JAVA_HOME for drools pdp
    Updates Policy API tests
    Updates XACML-PDP database connection
    
    Changes from Jim, Jorge and Chenfei
    
    Issue-ID: POLICY-2325
    Change-Id: I28ff3384f9ae9b7d1ce6940eb62aa87278e7a9c5
    Signed-off-by: Bilal A <bilal@research.att.com>

--- scripts/policy/policy-xacml-pdp/config/db/db.sh
+++ scripts/policy/policy-xacml-pdp/config/db/db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xv
 # ============LICENSE_START=======================================================
-#  Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+#  Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # ============LICENSE_END=========================================================
 
-for db in policyadmin
+for db in policyadmin operationshistory
 do
      mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" --execute "CREATE DATABASE IF NOT EXISTS ${db};"
      mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" --execute "GRANT ALL PRIVILEGES ON \`${db}\`.* TO '${MYSQL_USER}'@'%' ;"
