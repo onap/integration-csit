0337add9e1ad0c69cd4d1bdb0b03c05498af0eed
tragait <rahul.tyagi@est.tech>
2020-02-05 17:02:20 +0000

    fix for config-over-netconf tc
    Issue-ID: INT-1430
    Change-Id: Ic38607d9376b9ebb39bc0ebe18b23f5a73cb1b16
    Signed-off-by: tragait <rahul.tyagi@est.tech>

--- plans/usecases/config-over-netconf/teardown.sh
+++ plans/usecases/config-over-netconf/teardown.sh
@@ -4,6 +4,13 @@ SDNC_DOCKER_COMPOSE_PATH=$SDNC_DOCKER_PATH/docker-compose.yaml
 PNFSIM_DOCKER_COMPOSE_PATH=$INT_DOCKER_PATH/docker-compose.yml
 CDS_DOCKER_COMPOSE_PATH=$CDS_DOCKER_PATH/docker-compose.yaml
 
+echo "==========================bp logs =================================="
+docker logs bp-rest
+
+echo "==========================sdnc logs ================================"
+docker logs sdnc_controller_container
+
+
 docker-compose -f $SDNC_DOCKER_COMPOSE_PATH down
 docker-compose -f $PNFSIM_DOCKER_COMPOSE_PATH down
 docker-compose -f $CDS_DOCKER_COMPOSE_PATH down
