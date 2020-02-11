33aeb57f85228c2128df06ba930343f7eb9610aa
jhh <jorge.hernandez-herrero@att.com>
2020-02-10 18:56:00 -0600

    some clean up on drools-pdp csit
    Issue-ID: POLICY-2325
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>
    Change-Id: Iaf9622dd53d3acf4c5643881bb3c72e5822691ee
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>

--- plans/policy/drools-applications/setup.sh
+++ plans/policy/drools-applications/setup.sh
@@ -32,7 +32,9 @@ echo ${POLICY_DROOLS_APPS_VERSION_EXTRACT}
 export POLICY_DROOLS_APPS_VERSION="${POLICY_DROOLS_APPS_VERSION_EXTRACT:0:3}-SNAPSHOT-latest"
 echo ${POLICY_DROOLS_APPS_VERSION}
 
-docker login -u docker -p docker nexus3.onap.org:10001
+echo "user information: $(id)"
+echo "docker and docker-compose versions:"
+docker -v && docker-compose -v
 
 # Adding this waiting container to avoid race condition between api and mariadb containers.
 docker-compose -f ${WORKSPACE}/scripts/policy/docker-compose-drools-apps.yml run --rm start_dependencies
