7eb9317a4bccb7cbacf3f77e6e2871d07f66a0b9
jhh <jorge.hernandez-herrero@att.com>
2020-02-10 18:56:00 -0600

    some clean up on drools-pdp csit
    Issue-ID: POLICY-2325
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>
    Change-Id: Iaf9622dd53d3acf4c5643881bb3c72e5822691ee
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>

--- plans/policy/drools-pdp/setup.sh
+++ plans/policy/drools-pdp/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2017 AT&T Intellectual Property. All rights reserved.
+# Copyright 2017-2020 AT&T Intellectual Property. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,20 +15,63 @@
 # limitations under the License.
 #
 # Place the scripts in run order:
-source ${SCRIPTS}/common_functions.sh
 
-docker login -u docker -p docker nexus3.onap.org:10001
-docker pull nexus3.onap.org:10001/onap/policy-common-alpine:1.4.0
-docker tag nexus3.onap.org:10001/onap/policy-common-alpine:1.4.0 onap/policy-common-alpine:1.4.0
+# Select branch
+source ${SCRIPTS}/policy/config/policy-csit.conf
+export POLICY_MARIADB_VER
+echo ${GERRIT_BRANCH}
+echo ${POLICY_MARIADB_VER}
 
-source ${WORKSPACE}/scripts/policy/drools-pdp-script.sh
+echo "Uninstall docker-py and reinstall docker."
+pip uninstall -y docker-py
+pip uninstall -y docker
+pip install -U docker==2.7.0
 
-# Pass any variables required by Robot test suites in ROBOT_VARIABLES
-ROBOT_VARIABLES="-v IP:${IP} -v POLICY_IP:${POLICY_IP} -v PDP_IP:${PDP_IP} -v DOCKER_IP:${DOCKER_IP}" 
-export PDP_IP=${PDP_IP}
-export POLICY_IP=${POLICY_IP}
-export DOCKER_IP=${DOCKER_IP}
+sudo apt-get -y install libxml2-utils
+POLICY_DROOLS_VERSION_EXTRACT="$(curl -q --silent https://git.onap.org/policy/drools-pdp/plain/pom.xml?h=${GERRIT_BRANCH} | xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' -)"
+export POLICY_DROOLS_VERSION="${POLICY_DROOLS_VERSION_EXTRACT:0:3}-SNAPSHOT-latest"
+echo ${POLICY_DROOLS_VERSION}
 
-#Get current IP of VM
-HOST_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
-export HOST_IP=${HOST_IP}
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "user information: $(id)"
+echo "docker and docker-compose versions:"
+docker -v && docker-compose -v
+
+ls -alh ${WORKSPACE}/scripts/policy/config/drools/env ${WORKSPACE}/scripts/policy/config/drools ${WORKSPACE}/scripts/policy/config/
+
+sudo chown -R 1000:1000 ${WORKSPACE}/scripts/policy/config/drools
+sudo chmod 777 ${WORKSPACE}/scripts/policy/config/drools
+
+ls -alh ${WORKSPACE}/scripts/policy/config/drools/env ${WORKSPACE}/scripts/policy/config/drools ${WORKSPACE}/scripts/policy/config/
+
+export DEBUG=y
+docker-compose -f ${WORKSPACE}/scripts/policy/docker-compose-drools.yml up -d
+sleep 2m
+
+docker-compose logs
+docker logs mariadb
+docker logs drools
+docker container ls -a
+
+POLICY_DROOLS_IP=`get-instance-ip.sh drools`
+MARIADB_IP=`get-instance-ip.sh mariadb`
+
+echo DROOLS IP IS ${POLICY_DROOLS_IP}
+echo MARIADB IP IS ${MARIADB_IP}
+
+# Wait for initialization
+for i in {1..10}; do
+   curl -sS ${MARIADB_IP}:3306 && break
+   echo sleep $i
+   sleep $i
+done
+
+for i in {1..10}; do
+   curl -sS ${POLICY_DROOLS_IP}:6969 && break
+   echo sleep $i
+   sleep $i
+done
+
+ROBOT_VARIABLES="-v POLICY_DROOLS_IP:${POLICY_DROOLS_IP}"
