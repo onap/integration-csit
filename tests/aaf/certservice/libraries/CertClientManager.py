3ac52fcb49c5c30467754ccfcf2fb2b60cea10f7
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2020-03-19 14:51:51 +0100

    Verification of fields in trust/key store
    Issue-ID: AAF-1107
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>
    Change-Id: Ic5b897115980be19ca23f07778b5b9763764446c

--- tests/aaf/certservice/libraries/CertClientManager.py
+++ tests/aaf/certservice/libraries/CertClientManager.py
@@ -3,10 +3,17 @@ import os
 import shutil
 import re
 from OpenSSL import crypto
+from cryptography.x509.oid import ExtensionOID
+from cryptography import x509
 from docker.types import Mount
 
 ARCHIVES_PATH = os.getenv("WORKSPACE") + "/archives/"
 MOUNT_PATH = os.getenv("WORKSPACE") + "/tests/aaf/certservice/tmp"
+
+KEYSTORE_PASS_PATH = MOUNT_PATH + '/keystore.pass'
+KEYSTORE_JKS_PATH = MOUNT_PATH + '/keystore.jks'
+TRUSTSTORE_PASS_PATH = MOUNT_PATH + '/truststore.pass'
+TRUSTSTORE_JKS_PATH = MOUNT_PATH + '/truststore.jks'
 
 ERROR_API_REGEX = 'Error on API response.*[0-9]{3}'
 RESPONSE_CODE_REGEX = '[0-9]{3}'
@@ -40,6 +47,21 @@ class CertClientManager:
                 r_list.append(line)
         return r_list
 
+    def remove_nones_from_dict(self, dictionary):
+        return dict((k, v) for k, v in dictionary.iteritems() if k is not None)
+
+    def get_mappings(self):
+        return {'COMMON_NAME':'CN', 'ORGANIZATION':'O', 'ORGANIZATION_UNIT':'OU', 'LOCATION':'L', 'STATE':'ST', 'COUNTRY':'C', 'SANS':'SANS'}
+
+    def get_list_of_pairs_by_mappings(self, list):
+        mappings = self.get_mappings()
+        listOfEnvs = map(lambda k: k.split('='), list)
+        return dict((mappings.get(a[0]), a[1]) for a in listOfEnvs)
+
+    def get_envs_as_dict(self, list):
+        envs = self.get_list_of_pairs_by_mappings(list)
+        return self.remove_nones_from_dict(envs)
+
     def remove_client_container_and_save_logs(self, container_name, log_file_name):
         client = docker.from_env()
         container = client.containers.get(container_name)
@@ -50,23 +72,38 @@ class CertClientManager:
         self.remove_mount_dir()
 
     def can_open_keystore_and_truststore_with_pass(self):
-        keystore_pass_path = MOUNT_PATH + '/keystore.pass'
-        keystore_jks_path = MOUNT_PATH + '/keystore.jks'
-        can_open_keystore = self.can_open_jks_file_by_pass_file(keystore_pass_path, keystore_jks_path)
-
-        truststore_pass_path = MOUNT_PATH + '/truststore.pass'
-        truststore_jks_path = MOUNT_PATH + '/truststore.jks'
-        can_open_truststore = self.can_open_jks_file_by_pass_file(truststore_pass_path, truststore_jks_path)
+        can_open_keystore = self.can_open_jks_file_by_pass_file(KEYSTORE_PASS_PATH, KEYSTORE_JKS_PATH)
+        can_open_truststore = self.can_open_jks_file_by_pass_file(TRUSTSTORE_PASS_PATH, TRUSTSTORE_JKS_PATH)
 
         return can_open_keystore & can_open_truststore
 
+    def get_certificate(self, pass_file_path, jks_file_path):
+        password = open(pass_file_path, 'rb').read()
+        crypto.load_pkcs12(open(jks_file_path, 'rb').read(), password)
+        return crypto.load_pkcs12(open(jks_file_path, 'rb').read(), password).get_certificate()
+
+    def get_owner_data_from_certificate(self, certificate):
+        list = certificate.get_subject().get_components()
+        return dict((k, v) for k, v in list)
+
     def can_open_jks_file_by_pass_file(self, pass_file_path, jks_file_path):
-        try:
-            password = open(pass_file_path, 'rb').read()
-            crypto.load_pkcs12(open(jks_file_path, 'rb').read(), password)
-            return True
-        except:
-            return False
+      try:
+        self.get_certificate(pass_file_path, jks_file_path)
+        return True
+      except:
+        return False
+
+    def get_sans(self, cert):
+        extension = cert.to_cryptography().extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
+        dnsList = extension.value.get_values_for_type(x509.DNSName)
+        return ':'.join(map(lambda dns: dns.encode('ascii','ignore'), dnsList))
+
+    def owner_data_match_envs(self, path_to_env):
+        envs = self.get_envs_as_dict(self.read_list_env_from_file(path_to_env))
+        certificate = self.get_certificate(KEYSTORE_PASS_PATH, KEYSTORE_JKS_PATH)
+        data = self.get_owner_data_from_certificate(certificate)
+        data['SANS'] = self.get_sans(certificate)
+        return cmp(envs, data) == 0
 
     def create_mount_dir(self):
         if not os.path.exists(MOUNT_PATH):
