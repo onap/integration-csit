f340ddbd8422bf51dd537b851ebda62d25b1826f
Jim Hahn <jrh3@att.com>
2020-04-15 08:43:54 -0400

    End-to-end policy CSIT
    Modified the CSIT for drools-apps to use more components:
    - api to create policies
    - pap to deploy policies
    
    Also removed tabs.
    
    Issue-ID: POLICY-2491
    Change-Id: Iacd8d1953068bb718a63de8813dd1c307c21b1f8
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- plans/policy/drools-applications/setup.sh
+++ plans/policy/drools-applications/setup.sh
@@ -34,6 +34,10 @@ pip install -U docker==2.7.0
 sudo apt-get -y install libxml2-utils
 ${SCRIPTS}/policy/policy-models-simulators.sh
 
+POLICY_API_VERSION_EXTRACT="$(curl -q --silent https://git.onap.org/policy/api/plain/pom.xml?h=${GERRIT_BRANCH} | xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' -)"
+export POLICY_API_VERSION="${POLICY_API_VERSION_EXTRACT:0:3}-SNAPSHOT-latest"
+POLICY_PAP_VERSION_EXTRACT="$(curl -q --silent https://git.onap.org/policy/pap/plain/pom.xml?h=${GERRIT_BRANCH} | xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' -)"
+export POLICY_PAP_VERSION="${POLICY_PAP_VERSION_EXTRACT:0:3}-SNAPSHOT-latest"
 POLICY_XACML_PDP_VERSION_EXTRACT="$(curl -q --silent https://git.onap.org/policy/xacml-pdp/plain/pom.xml?h=${GERRIT_BRANCH} | xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' -)"
 export POLICY_XACML_PDP_VERSION="${POLICY_XACML_PDP_VERSION_EXTRACT:0:3}-SNAPSHOT-latest"
 POLICY_DROOLS_APPS_VERSION_EXTRACT="$(curl -q --silent https://git.onap.org/policy/drools-applications/plain/pom.xml?h=${GERRIT_BRANCH} | xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' -)"
@@ -50,31 +54,37 @@ docker -v && docker-compose -v
 # Adding this waiting container due to race condition between drools and mariadb
 docker-compose -f ${SCR2}/docker-compose-drools-apps.yml run --rm start_dependencies
 
+# Adding this waiting container due to race condition between pap and xacml
+docker-compose -f ${SCR2}/docker-compose-drools-apps.yml run --rm start_pap
+
 # now bring everything else up
 docker-compose -f ${SCR2}/docker-compose-drools-apps.yml run --rm start_all
 
 unset http_proxy https_proxy
 
 DROOLS_IP=`get-instance-ip.sh drools`
+API_IP=`get-instance-ip.sh policy-api`
+PAP_IP=`get-instance-ip.sh policy-pap`
 XACML_IP=`get-instance-ip.sh policy-xacml-pdp`
 SIM_IP=`get-instance-ip.sh policy.api.simpledemo.onap.org`
 export SIM_IP
 
 echo DROOLS IP IS ${DROOLS_IP}
+echo API IP IS ${API_IP}
+echo PAP IP IS ${PAP_IP}
 echo XACML IP IS ${XACML_IP}
 echo SIMULATORS IP IS ${SIM_IP}
-
-# activate xacml
-${SCR2}/manage.sh ${SCR2}/activate.xacml.json
 
 # give enough time for the controllers to come up
 sleep 15
 
-# wait for xacml to activate
-${SCRIPTS}/policy/wait_for_port.sh ${XACML_IP} 6969
+DATA=${WORKSPACE}/simulators/models/models-examples/src/main/resources/policies
 
 ROBOT_VARIABLES=""
 ROBOT_VARIABLES="${ROBOT_VARIABLES} -v SCR2:${SCR2}"
+ROBOT_VARIABLES="${ROBOT_VARIABLES} -v DATA:${DATA}"
 ROBOT_VARIABLES="${ROBOT_VARIABLES} -v DROOLS_IP:${DROOLS_IP}"
+ROBOT_VARIABLES="${ROBOT_VARIABLES} -v API_IP:${API_IP}"
+ROBOT_VARIABLES="${ROBOT_VARIABLES} -v PAP_IP:${PAP_IP}"
 ROBOT_VARIABLES="${ROBOT_VARIABLES} -v XACML_IP:${XACML_IP}"
 ROBOT_VARIABLES="${ROBOT_VARIABLES} -v SIM_IP:${SIM_IP}"
