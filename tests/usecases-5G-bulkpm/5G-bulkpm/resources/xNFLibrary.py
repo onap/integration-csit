f35d725778f54e2343e271b443d0e18da03a1955
efiacor <fiachra.corcoran@est.tech>
2020-05-06 01:14:00 +0100

    Refactoring 5G-bulk-pm suite
    Signed-off-by: efiacor <fiachra.corcoran@est.tech>
    Change-Id: I4b2ad0dc5147e10ffc55f4ec210a29f073cd865b
    Issue-ID: INT-832

--- tests/usecases-5G-bulkpm/5G-bulkpm/resources/xNFLibrary.py
+++ tests/usecases-5G-bulkpm/5G-bulkpm/resources/xNFLibrary.py
@@ -3,22 +3,27 @@ Created on Aug 18, 2017
 
 @author: sw6830
 '''
+import time
+import uuid
+
 from robot.api import logger
-from Queue import Queue
-import uuid, time, json, threading,os, platform, subprocess,paramiko
+
 
 class xNFLibrary(object):
 
     def __init__(self):
         pass
 
-    def create_header_from_string(self, dictStr):
+    @staticmethod
+    def create_header_from_string(dict_str):
         logger.info("Enter create_header_from_string: dictStr")
-        return dict(u.split("=") for u in dictStr.split(","))
+        return dict(u.split("=") for u in dict_str.split(","))
 
+    @staticmethod
     def Generate_UUID(self):
         """generate a uuid"""
         return uuid.uuid4()
+
 
 if __name__ == '__main__':
     lib = xNFLibrary()
