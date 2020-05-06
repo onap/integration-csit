12d80fa20a3440d6f701ab7f4638f8202a7718b2
efiacor <fiachra.corcoran@est.tech>
2020-05-06 01:14:00 +0100

    Seperating usecase test suite dependencies
      # Refactoring BulkPM Dataplane test suite
    
    Signed-off-by: efiacor <fiachra.corcoran@est.tech>
    Change-Id: I4b2ad0dc5147e10ffc55f4ec210a29f073cd865b
    Issue-ID: INT-832

---
+++ plans/usecases-5G-bulkpm/5G-bulkpm/teardown.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Starting teardown script"
+kill-instance.sh dmaap-message-router-server
+kill-instance.sh dmaap-message-router-kafka
+kill-instance.sh dmaap-message-router-zookeeper
+kill-instance.sh dmaap-datarouter-node
+kill-instance.sh dmaap-datarouter-prov
+kill-instance.sh dmaap-dr-prov-mariadb
+kill-instance.sh fileconsumer-node
+kill-instance.sh dcaegen2-vescollector
+kill-instance.sh dcaegen2-datafile-collector
+kill-instance.sh dcaegen2-pm-mapper
+kill-instance.sh sftp
+kill-instance.sh config-binding-service-sim
+yes | docker network prune
