a8c96d35198ce9d98053703b796a5cead561e292
BjornMagnussonXA <bjorn.magnusson@est.tech>
2020-09-07 14:06:35 +0200

    CSIT Policy Management
    CSIT test case using FTC1 and applicable ONAP images
    
    Change-Id: I7d3e382644f1b2e1de320931e24068976515cd4f
    
    Issue-ID: CCSDK-2700
    
    Signed-off-by: BjornMagnussonXA <bjorn.magnusson@est.tech>
    Change-Id: I0571b3eb857b0347963af2b982a7de79f0ea78da

---
+++ plans/ccsdk/polmansuite/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#  ============LICENSE_START===============================================
+#  Copyright (C) 2020 Nordix Foundation. All rights reserved.
+#  ========================================================================
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ============LICENSE_END=================================================
+
+
+cd $WORKSPACE/archives
+
+git clone "https://gerrit.o-ran-sc.org/r/nonrtric"
+
+AUTOTEST_ROOT=$WORKSPACE/archives/nonrtric/test/auto-test
+
+# Temporary solution to setup the Non-RT RIC components to point to ONAP images and tags
+# Shall be removed when the Non-RT RIC test env is moved/copied to ONAP
+cp $WORKSPACE/plans/ccsdk/polmansuite/test_env.sh $WORKSPACE/archives/nonrtric/test/common
+
+# Temporary solution to not test with the SDNC image
+cp $WORKSPACE/plans/ccsdk/polmansuite/FTC1.sh $WORKSPACE/archives/nonrtric/test/auto-test/FTC1.sh
+
+#Make the env vars availble to the robot scripts
+ROBOT_VARIABLES="-b debug.log -v AUTOTEST_ROOT:${AUTOTEST_ROOT}"
--- tests/ccsdk/oran/vCPE/__init__.robot
+++
@@ -1,2 +0,0 @@
-*** Settings ***
-Documentation    Ccsdk Oran - vCPE
