c334916cec707b28332d8fa44688944b56167f53
Jim Hahn <jrh3@att.com>
2020-09-17 17:03:34 -0400

    Make stand-alone Policy environment
    This is a first cut at making it possible to easily run Policy
    components within a stand-alone environment.  Made the following
    changes:
    - enhanced wait_for_port.sh to accept a list of hosts/ports
    - enhanced wait_for_port.sh to accept an optional command to execute
      when the wait completes
    - consolidated all of the yml files into a single file
    - removed "extra" services from the yml so that a component and its
      dependents can be brought up in a single invocation
    - modified the component setup.sh
    - Updated the csar file used for policy-distribution as per the latest
      changes to remove legacy apex operational policy type from PAP
    
    With these changes, a developer can clone the csit repo, run detmVers.sh
    to set the docker image version env variables, and then bring up a
    component (and its dependents) using:
    
      docker-compose -f scripts/policy/docker-compose-all.yml
        up --detach <component>
    
    Note: this does not preload or deploy any policies; that must be done
    manually via curl.
    
    Modified to use new simulator docker image.
    Also fixed drools-apps CSIT broken due to disabling frankfurt
    controller.
    Also removed operational.Apex policy.
    
    Issue-ID: POLICY-2742
    Change-Id: I412dbd5db9219ad2cdece9693f0b4b9fe7b2eb69
    Signed-off-by: Jim Hahn <jrh3@att.com>
    Signed-off-by: a.sreekumar <ajith.sreekumar@bell.ca>

--- plans/policy/pap/setup.sh
+++ plans/policy/pap/setup.sh
@@ -19,39 +19,20 @@
 # ============LICENSE_END=========================================================
 
 source ${SCRIPTS}/policy/config/policy-csit.conf
-export POLICY_MARIADB_VER
-echo ${GERRIT_BRANCH}
-echo ${POLICY_MARIADB_VER}
 
 echo "Uninstall docker-py and reinstall docker."
 pip uninstall -y docker-py
 pip uninstall -y docker
 pip install -U docker==2.7.0
 
-
 sudo apt-get -y install libxml2-utils
-bash ${SCRIPTS}/policy/policy-models-dmaap-sim.sh
+bash ${SCRIPTS}/policy/get-models-examples.sh
 
-POLICY_API_VERSION_EXTRACT="$(curl -q --silent https://git.onap.org/policy/api/plain/pom.xml?h=${GERRIT_BRANCH} | xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' -)"
-export POLICY_API_VERSION="${POLICY_API_VERSION_EXTRACT:0:3}-SNAPSHOT-latest"
-POLICY_PAP_VERSION_EXTRACT="$(curl -q --silent https://git.onap.org/policy/pap/plain/pom.xml?h=${GERRIT_BRANCH} | xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' -)"
-export POLICY_PAP_VERSION="${POLICY_PAP_VERSION_EXTRACT:0:3}-SNAPSHOT-latest"
+source ${SCRIPTS}/policy/detmVers.sh
 
-echo ${POLICY_API_VERSION}
-echo ${POLICY_PAP_VERSION}
-
-# Adding this waiting container due to race condition between pap and mariadb
-docker-compose -f ${WORKSPACE}/scripts/policy/docker-compose-pap.yml run --rm start_dependencies
-
-#Configure the database
-docker exec --tty mariadb  chmod +x /docker-entrypoint-initdb.d/db.sh
-docker exec --tty mariadb  /docker-entrypoint-initdb.d/db.sh
-
-# now bring everything else up
-docker-compose -f ${WORKSPACE}/scripts/policy/docker-compose-pap.yml run --rm start_all
+docker-compose -f ${SCRIPTS}/policy/docker-compose-all.yml up -d pap
 
 unset http_proxy https_proxy
-
 
 POLICY_PAP_IP=`get-instance-ip.sh policy-pap`
 POLICY_API_IP=`get-instance-ip.sh policy-api`
@@ -61,7 +42,12 @@ echo PAP IP IS ${POLICY_PAP_IP}
 echo API IP IS ${POLICY_API_IP}
 echo MARIADB IP IS ${MARIADB_IP}
 
-DATA=${WORKSPACE}/dmaap-sim/models/models-examples/src/main/resources/policies
+
+# wait for the app to start up
+${SCRIPTS}/policy/wait_for_port.sh ${POLICY_PAP_IP} 6969
+
+
+DATA=${WORKSPACE}/models/models-examples/src/main/resources/policies
 
 ROBOT_VARIABLES=""
 ROBOT_VARIABLES="${ROBOT_VARIABLES} -v POLICY_PAP_IP:${POLICY_PAP_IP}"
