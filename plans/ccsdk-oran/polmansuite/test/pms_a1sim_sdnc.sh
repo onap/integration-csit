5933539a229ebbfb0304ae969489bb83547e11db
ecaiyanlinux <martin.c.yan@est.tech>
2021-01-21 14:41:14 +0100

    Update csit for nonrtric
    Change-Id: I6f4da7cf2610fd56bb9d7e7f2fdea8a5b7e9489a
    Signed-off-by: ecaiyanlinux <martin.c.yan@est.tech>
    Issue-ID: CCSDK-3090

---
+++ plans/ccsdk-oran/polmansuite/test/pms_a1sim_sdnc.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+#  ============LICENSE_START===============================================
+#  Copyright (C) 2020 Nordix Foundation. All rights reserved.
+#  ========================================================================
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ============LICENSE_END=================================================
+
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+docker stop $(docker ps -aq)
+docker system prune -f
+
+cd ${SHELL_FOLDER}/../config
+cp application_configuration.json.sdnc application_configuration.json
+
+cd ${SHELL_FOLDER}/../
+./docker-compose -f docker-compose.yml -f sdnc/docker-compose.yml up -d
+
+# Healthcheck docker containers
+
+# check SIM1 status
+for i in {1..20}; do
+    echo "check SIM1 status:"
+    curlString="curl -skw %{http_code} http://localhost:30001/"
+    res=$($curlString)
+    echo "$res"
+    expect="OK200"
+    if [ "$res" == "$expect" ]; then
+        echo -e "SIM1 is alive!\n"
+        break;
+    else
+        sleep $i
+    fi
+done
+
+# check SIM2 status
+for i in {1..20}; do
+    echo "check SIM2 status:"
+    curlString="curl -skw %{http_code} http://localhost:30003/"
+    res=$($curlString)
+    echo "$res"
+    expect="OK200"
+    if [ "$res" == "$expect" ]; then
+        echo -e "SIM2 is alive!\n"
+        break;
+    else
+        sleep $i
+    fi
+done
+
+# check SIM3 status
+for i in {1..20}; do
+    echo "check SIM3 status:"
+    curlString="curl -skw %{http_code} http://localhost:30005/"
+    res=$($curlString)
+    echo "$res"
+    expect="OK200"
+    if [ "$res" == "$expect" ]; then
+        echo -e "SIM3 is alive!\n"
+        break;
+    else
+        sleep $i
+    fi
+done
+
+# check PMS status
+for i in {1..20}; do
+    echo "check PMS status::"
+    curlString="curl -skw %{http_code} http://localhost:8001/status/"
+    res=$($curlString)
+    echo "$res"
+    expect="hunky dory200"
+    if [ "$res" == "$expect" ]; then
+        echo -e "PMS is alive!\n"
+        break;
+    else
+        sleep $i
+    fi
+done
+
+# check SDNC status
+for i in {1..20}; do
+    echo "check SDNC status:"
+    curlString="curl -s -o /dev/null -I -w %{http_code} http://localhost:8202/apidoc/explorer//"
+    res=$($curlString)
+    echo "$res"
+    expect="200"
+    if [ "$res" == "$expect" ]; then
+        echo -e "SDNC is alive!\n"
+        break;
+    else
+        sleep $i
+    fi
+done
+
+cd ${SHELL_FOLDER}/../data
+./preparePmsData.sh
+
