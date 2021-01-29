a2903406f529e77f181d90ee0eee62c855961257
efiacor <fiachra.corcoran@est.tech>
2021-01-29 14:28:34 +0000

    [PMSH] Update CSITs to use DMaaP MR
    Signed-off-by: efiacor <fiachra.corcoran@est.tech>
    Change-Id: I1be0373cffde124a4a20cf0530cd756389eec532
    Issue-ID: DCAEGEN2-2186

--- plans/dcaegen2-services-pmsh/testsuite/teardown.sh
+++ plans/dcaegen2-services-pmsh/testsuite/teardown.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Starting teardown script"
+source ${WORKSPACE}/scripts/dmaap-message-router/dmaap-mr-teardown.sh
+dmaap_mr_teardown
 TEST_PLANS_DIR=$WORKSPACE/plans/dcaegen2-services-pmsh/testsuite
 mkdir -p $WORKSPACE/archives
 docker exec pmsh /bin/sh -c "cat /var/log/ONAP/dcaegen2/services/pmsh/*"
