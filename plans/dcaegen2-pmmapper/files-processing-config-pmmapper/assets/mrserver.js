2bdbe490c400a5b465ac1803cc91d95915ecb52d
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2021-02-16 09:34:13 +0100

    Add CSITs for Pm-mapper files processing performance
    Issue-ID: DCAEGEN2-2600
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>
    Change-Id: Id7e4d345380fd5fd08f28a3ddff04a4fde8f7730

---
+++ plans/dcaegen2-pmmapper/files-processing-config-pmmapper/assets/mrserver.js
@@ -0,0 +1,26 @@
+var httpServer = function() {
+var http = require('http'),
+url = require('url'),
+fs = require('fs'),
+
+start = function(port) {
+    var server = http.createServer(function(req, res) {
+    processHttpRequest(res);	
+    });
+    server.listen(port, function() {
+        console.log('Listening on ' + port + '...');
+    });
+},
+
+processHttpRequest = function(res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    console.log('received message')
+    res.end('Published Successfully.\n');
+};
+
+return {
+    start: start		
+}
+}();
+
+httpServer.start(3904);
