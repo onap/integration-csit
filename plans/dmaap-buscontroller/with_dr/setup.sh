f4f1f421bd4a16e8cccfa22daf96c66accd30f73
Lasse Kaihlavirta <l.kaihlavirt@partner.samsung.com>
2021-02-23 11:39:11 +0200

    Remove mock startups from DMaaP CSIT suites
     - remove irrelevant mock startups and teardowns
     - remove unused positional DRPS and MRC IP shell arguments
       from dmaapbc-init.sh
    
    Issue-ID: DMAAP-1552
    Signed-off-by: Lasse Kaihlavirta <l.kaihlavirt@partner.samsung.com>
    Change-Id: Ibba7823662153b5b3880e1bb58a13bd77b6232ba

--- plans/dmaap-buscontroller/with_dr/setup.sh
+++ plans/dmaap-buscontroller/with_dr/setup.sh
@@ -4,6 +4,7 @@
 # org.onap.dmaap
 # ================================================================================
 # Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+# Modification copyright (C) 2021 Samsung Electronics Co., Ltd.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -33,25 +34,19 @@ else
 	dmaap_dr_launch
 	DRPS_IP=${IP}
 
-	#source ${WORKSPACE}/scripts/dmaap-buscontroller/start-mock.sh
-	#start_mock "aaf" 
-	#AAF_IP=${IP}
 	AAF_IP=0.0.0.0
-	#start_mock "drps" 
-	#DRPS_IP=${IP}
 	MRC_IP=0.0.0.0
 
 	source ${WORKSPACE}/scripts/dmaap-buscontroller/dmaapbc-launch.sh 
 	dmaapbc_launch $AAF_IP $MRC_IP $DRPS_IP
 	DMAAPBC_IP=${IP}
 
-
 	echo "AAF_IP=$AAF_IP MRC_IP=$MRC_IP DRPS_IP=$DRPS_IP DMAAPBC_IP=$DMAAPBC_IP"
 
 	# Pass any variables required by Robot test suites in ROBOT_VARIABLES
-	ROBOT_VARIABLES="-v AAF_IP:${AAF_IP} -v MRC_IP:${MRC_IP} -v DRPS_IP:${DRPS_IP} -v DMAAPBC_IP:${DMAAPBC_IP}"
+	ROBOT_VARIABLES="-v MRC_IP:${MRC_IP} -v DRPS_IP:${DRPS_IP} -v DMAAPBC_IP:${DMAAPBC_IP}"
 	set -x
-	${WORKSPACE}/scripts/dmaap-buscontroller/dmaapbc-init.sh ${DMAAPBC_IP} ${DRPS_IP} ${MRC_IP} https
+	${WORKSPACE}/scripts/dmaap-buscontroller/dmaapbc-init.sh ${DMAAPBC_IP} https
 	set +x
 fi
 
